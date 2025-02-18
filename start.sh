16619df6b20645e7aff735b888cd54a41ae6f5ee
smalli <809885126@qq.com>
2025-02-18 08:51:03 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 source ~/miniconda3/bin/activate sdf
 python launch.py --listen --port 7860 \
 --models-dir ~/SoftWare/Sd/models/ \
---enable-insecure-extension-access --precision full --no-half --xformers
+--enable-insecure-extension-access --xformers #--precision full --no-half
 #--ckpt-dir ~/SoftWare/Sd/models/Stable-diffusion \
 #--gfpgan-dir ~/SoftWare/Sd/models/GFPGAN \
 #--clip-models-path ~/SoftWare/Sd/models/openai \
